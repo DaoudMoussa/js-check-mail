6ded4e5526ed4b46b939b1ab2ac8fdc3f6520b0a
Daoud <99m.daoud@gmail.com>
2020-10-14 14:46:03 +0200

    Aggiunta stampa in HTML

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // Esercitazione 1: Check lista email
 var accesso = false;
 // Creare un array con un insieme di indirizzi email.
-var emails = ["pippo@disney.com", "pluto@disney.com", "topolino@disney.com", "paperino@disney.com"]
+var emails = ["pippo@disney.com", "pluto@disney.com", "topolino@disney.com", "paperino@disney.com"];
 // Chiedere all'utente il suo indirizzo email e verificare se è nella lista (tramite un ciclo for).
 var emailUtente = prompt("Inserisci la tua mail");
 
@@ -12,7 +12,7 @@ for (var i = 0; i < emails.length; i++) {
 }
 // Comunicare quindi un messaggio appropriato (sei in lista/non sei in lista)
 if (accesso) {
-    console.log("Accesso consentito");
+    document.getElementById('messaggio').innerHTML = "Accesso consentito";
 } else {
-    console.log("accesso negato");
+    document.getElementById('messaggio').innerHTML = "Accesso negato";
 }
